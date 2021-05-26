fb4210faeb32057a733c288a18d10e5c2b15fa80
Max Block <maxblock@pm.me>
2021-05-26 20:41:17 +0300

    update

--- main.js
+++ main.js
@@ -18,7 +18,12 @@ let buffer = borsh.serialize(schema, value);
 console.log(new Int32Array(buffer).toString())
 // 10,3,0,0,0,0,0,0,3,0,0,0,98,108,97
 
-// value = new Test();
-// schema = new Map([[Test, {kind: 'enum'}]]);
-// buffer = borsh.serialize(schema, value);
-// console.log(new Int32Array(buffer).toString())
+value = new Test({
+    admin: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 100, 120],
+    min_bet: 100,
+    max_bet: 7000
+});
+schema = new Map([[Test, {kind: 'struct', fields: [["admin", [32]], ["min_bet", "u64"], ["max_bet", "u64"]]}]]);
+buffer = borsh.serialize(schema, value);
+console.log(new Int32Array(buffer).toString())
+// 1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,100,120,100,0,0,0,0,0,0,0,88,27,0,0,0,0,0,0
